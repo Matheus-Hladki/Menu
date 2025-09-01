388353214f8ca166571322d365e1ecf15333b595
Matheus Hladki <Matheus.Hladki@correo.ucu.edu.uy>
2025-09-01 11:34:20 -0300

    Table class update

--- src/Table.cs
+++ src/Table.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Ucu.Poo.Restaurant
@@ -8,10 +9,30 @@ namespace Ucu.Poo.Restaurant
     public class Table
     {
         private List<Dish> order = new List<Dish>();
+        private bool IsOccupied;
+        private int Number;
+        
+        public void Occupy()
+        {
+            this.IsOccupied = true;
+        }
 
+        public void Free()
+        {
+            this.IsOccupied = false;
+            order.Clear();
+        }
+
+        public void AddToOrder(Dish dish)
+        {
+            order.Add(dish);
+            Console.WriteLine("Order Added Successfully");
+        }
         public bool HasOrders()
         {
             return this.order.Count > 0;
         }
+        
+        
     }
 }
