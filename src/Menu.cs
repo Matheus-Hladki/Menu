0941a3e6b7ac4c06bf84622c70fa39117ce89c21
Geronimo <GERONIMO.SOSA@correo.ucu.edu.uy>
2025-09-01 11:59:43 -0300

    t 

--- src/Menu.cs
+++ src/Menu.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Ucu.Poo.Restaurant;
 
 namespace Ucu.Poo.Restaurant
 {
@@ -9,5 +10,29 @@ namespace Ucu.Poo.Restaurant
     public class Menu
     {
         private List<Dish> dishes = new List<Dish>();
+        // Agregar un platillo a la lista de platillos del menú.
+        public void AddDish(Dish dish)
+        {
+            dishes.Add(dish);
+        }
+
+// Eliminar un platillo de la lista del menú.
+        public void RemoveDish(Dish dish)
+        {
+            dishes.Remove(dish);
+        }
+
+// Buscar y devolver un platillo específico del menú basado en su nombre; retorna null si no encuentra en platillo.
+        public Dish GetDishByName(string name)
+        {
+            foreach (Dish dish in dishes)
+            {
+                if (dish.name == name)
+                    return dish;
+            }
+
+            return null;
+        }
     }
 }
+
